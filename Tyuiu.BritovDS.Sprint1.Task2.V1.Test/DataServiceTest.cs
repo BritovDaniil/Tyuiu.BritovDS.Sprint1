9cd1e00f9df0b3c4087d9384f9c78cbe857865a4
user <user@WIN-FJ5S0KDD7IV>
2025-09-21 23:31:04 +0300

    Task2

---
+++ Tyuiu.BritovDS.Sprint1.Task2.V1.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BritovDS.Sprint1.Task2.V1.Lib;
+
+namespace Tyuiu.BritovDS.Sprint1.Task2.V1.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            double res = ds.ConvertKmToM(x);
+            Assert.AreEqual(0.622, res);
+        }
+    }
+}
---
+++ Tyuiu.BritovDS.Sprint1.Task2.V1/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.BritovDS.Sprint1.Task2.V1.Lib;
+
+namespace Tyuiu.BritovDS.Sprint1.Task2.V1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Бритов Д. С. | ИСПб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #1                                                              *");
+            Console.WriteLine("* Выполнил: Бритов Д. С. | ИСПб-25-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* вычисляет расстояние в милях и печатает его на экране.                  *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Введите значение X:");
+            var x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine($"Расстояние в милях = {ds.ConvertKmToM(x):F3}");
+            Console.ReadKey();
+        }
+    }
+}
