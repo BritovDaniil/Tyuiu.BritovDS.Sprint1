5a689ae3c0379f92d302d14a3513acb015c3f8e8
user <user@WIN-FJ5S0KDD7IV>
2025-09-27 14:45:53 +0300

    Task3

---
+++ Tyuiu.BritovDS.Sprint1.Task3.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Tyuiu.BritovDS.Sprint1.Task3.V6.Lib;
+
+namespace Tyuiu.BritovDS.Sprint1.Task3.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+
+            double distance = 67;
+            double gasFlow = 8.5;
+            double gasPrice = 6.5;
+
+            double expected = 74.035;
+            double result = ds.TravelCost(distance, gasFlow, gasPrice);
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
